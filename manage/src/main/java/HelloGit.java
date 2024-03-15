abdc050090306263a90ace270969d506ac821c93
qimingxingx <1239535565@qq.com>
2024-03-15 20:29:57 +0800

    测试Git提交

---
+++ manage/src/main/java/HelloGit.java
@@ -0,0 +1,6 @@
+/**
+ * @AUTHOR 666
+ * @CREAT 2024-03-15-20:25
+ */
+public class HelloGit {
+}
--- manage/src/main/java/com/hbnu/ManageApplication.java
+++ manage/src/main/java/com/hbnu/ManageApplication.java
@@ -3,7 +3,6 @@ package com.hbnu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.hbnu.dao")/**dao层接口扫描*/
@@ -12,5 +11,4 @@ public class ManageApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
     }
-
 }
